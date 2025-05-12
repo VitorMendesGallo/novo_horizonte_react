fd23069fac37e87cd7f3e25106abdc50e06dc202
Vitor Mendes <vitormendesmoreiragallo@gmail.com>
2025-05-11 22:49:38 -0300

    hospedagem

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'novo-horizonte-react-vitor',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'novo-horizonte-react-vitor',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
